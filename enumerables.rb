3823c332d310b02b8b9bf692770f3e655f008fb6
sz16900 <sz16900@my.bristol.ac.uk>
2020-02-03 17:55:28 -0500

    :sparkles: my_select
    add my_select, refactored code, add more tests, add return enumerables

--- enumerables.rb
+++ enumerables.rb
@@ -1,19 +1,33 @@
 module Enumerable
   def my_each
-    i = 0
-    while i < size
-      yield(self[i])
-      i += 1
+    return enum_for(:my_each) unless block_given?
+    index = 0
+    while index < size
+      yield(self[index])
+      index += 1
     end
     self
   end
 
   def my_each_with_index
-    i = 0
-    while i < size
-      yield(self[i], i)
-      i += 1
+    return enum_for(:my_each_with_index) unless block_given?
+    index = 0
+    while index < size
+      yield(self[index], index)
+      index += 1
     end
     self
   end
+
+  def my_select
+    select_array = []
+    return enum_for(:my_select) unless block_given?
+    self.my_each do |x|
+        if yield x
+            select_array << x
+        end
+    end
+    select_array
+  end
+
 end
--- testing.rb
+++ testing.rb
@@ -1,11 +1,22 @@
 require './enumerables.rb'
 
+test_array_1 = [11, 2, 3, 56]
+test_array_2 = %w[a b c d]
+
 # my_each
 p 'my_each'
-[11, 2, 3, 56].my_each { |x| p x }
-%w[a b c d].my_each { |x| p x }
+test_array_1.my_each { |x| p x }
+test_array_2.my_each { |x| p x }
+p test_array_2.my_each 
 
 # my_each_with_index
 p 'my_each-with_index'
-[11, 2, 3, 56].my_each_with_index { |x, y| p "item: #{x}, index: #{y}" }
-%w[john juan carl].my_each_with_index { |x, y| p "item: #{x}, index: #{y}" }
+test_array_1.my_each_with_index { |x, y| p "item: #{x}, index: #{y}" }
+test_array_2.my_each_with_index { |x, y| p "item: #{x}, index: #{y}" }
+p test_array_2.my_each_with_index 
+
+# my_select
+p "my_select"
+p test_array_1.my_select {|x| x.even?}
+p test_array_2.my_select {|x| x == "c"}
+p test_array_2.my_select
