54f1efa923de4bb93c01f91c0b5ae013c8268926
sz16900 <sz16900@my.bristol.ac.uk>
2020-02-03 16:01:58 -0500

    :sparkles: my_each
    add the my_each feature with testing

---
+++ enumerables.rb
@@ -0,0 +1,12 @@
+module Enumerable
+    
+    def my_each
+        i = 0
+        while i < self.size
+            yield(self[i])  
+            i+=1      
+        end
+        self
+    end
+
+end
---
+++ testing.rb
@@ -0,0 +1,11 @@
+require "./enumerables.rb"
+
+# my_each
+p "my_each"
+[11,2,3,56].my_each {|x| p x}
+['a', 'b', 'c', 'd'].my_each {|x| p x}
+
+# my_each_with_index
+p "my_each-with_index"
+[11,2,3,56].my_each_with_index {|x, y| p "item: #{x}, index: #{y}"}
+['john', 'juan', 'carl'].my_each_with_index {|x, y| p "item: #{x}, index: #{y}"}
