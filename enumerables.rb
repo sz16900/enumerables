01a5b7a8c080bfd2beb4cb61f640333a6efe8a80
sz16900 <sz16900@my.bristol.ac.uk>
2020-02-03 16:23:00 -0500

    :sparkles: my_each_with_index
    add my_each with index

--- enumerables.rb
+++ enumerables.rb
@@ -9,4 +9,13 @@ module Enumerable
         self
     end
 
+    def my_each_with_index
+        i = 0
+        while i < self.size
+            yield(self[i], i)  
+            i+=1      
+        end
+        self
+    end
+    
 end
